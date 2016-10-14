ca66521c1cee98aa09adc468d8a3a1c0ec9656c8
Anthony <theanthonyfalcon@gmail.com>
2016-10-14 14:19:59 -0700

    really added the images this time... >_<

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -31,7 +31,7 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 });
 
 gulp.task('useminTrigger', ['deleteDistFolder'], function() {
-    gulp.start("usemin")
+    gulp.start("usemin");
 });
 
 gulp.task("usemin", ['styles', 'scripts'], function() {
